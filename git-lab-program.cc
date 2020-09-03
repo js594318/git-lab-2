bb2f536422761bc657a8acf5e14ac4965ac84f21
Jay Schaffer <js594318@ohio.edu>
2020-09-03 16:55:52 -0400

    the program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!"<<endl;
+    return 0;
+}// main
